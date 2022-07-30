20385d923309799badcf0cb3943d83e4c14e118c
fadhilmufid <mufidfadhil@gmail.com>
2022-07-30 15:48:41 +0700

    Open File

---
+++ 22_open_file.rb
@@ -0,0 +1,20 @@
+#how to open file
+File.open("22_open_file.txt", "r") do |file|
+
+    #puts file.readlines()
+    for line in file.readlines()
+        puts line
+    end
+     
+end 
+
+#store with variable (but it didnt close the file)
+#how to open file
+puts "------------"
+file = File.open("22_open_file.txt", "r")
+
+for line in file.readlines()
+    puts "Second Method= "+ line
+end
+
+file.close()
