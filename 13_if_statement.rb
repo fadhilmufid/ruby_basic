668f6710588fe0da645dd1914af83f350f3a25c5
fadhilmufid <mufidfadhil@gmail.com>
2022-07-30 13:56:00 +0700

    If Statement Comparison

--- 13_if_statement.rb
+++ 13_if_statement.rb
@@ -1,6 +1,5 @@
 ismale = true
 
-
 if ismale
     puts "You are male"
 else
---
+++ 14_if_statement_2.rb
@@ -0,0 +1,23 @@
+#comparison in if statement
+def max(num1, num2, num3)
+    if num1 >= num2 and num1=>num3
+        return num1 
+    elsif num2>=num1 and num2>=num3
+        return num2
+    else
+        return num3
+    end
+end
+
+puts max(4, 50, 3)
+
+#there are other comparison operation
+# == *equal to
+# != *not equal to
+# > *greater than
+# < *less than
+# >= *greater than equal to
+# <= *less than equal to
+
+#we can also use string
+
