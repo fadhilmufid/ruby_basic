f09b29fb8f1ba453b09ee8b87bf0bc5bc6021be8
fadhilmufid <mufidfadhil@gmail.com>
2022-07-30 11:22:49 +0700

    Data Types

---
+++ 05_data_types.rb
@@ -0,0 +1,11 @@
+#text form (string)
+name = "Mufid"
+occupation = "Software Engineer"
+#number (Integer)
+age  = 23
+#decimal (floating point number)
+gpa = 2.9
+#True or False (boolean)
+ismale = true
+isfat = false
+flaws = nil
