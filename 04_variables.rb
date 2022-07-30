f7eb45bd800f7ddd00a042106adb4c6c495c1daf
fadhilmufid <mufidfadhil@gmail.com>
2022-07-30 11:16:32 +0700

    Variables

---
+++ 04_variables.rb
@@ -0,0 +1,11 @@
+character_name = "Fadhil"
+character_age = "23"
+
+
+puts ("My name is " + character_name)
+puts ("I am " +character_age +" Years Old")
+
+character_name = "Mufid"
+character_age = "17"
+puts ("People also call me " +character_name)
+puts ("But my mood is like "+character_age+ " years old") 
