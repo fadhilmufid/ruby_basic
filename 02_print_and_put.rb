f7f7588d3ef4093d40d6518afcfaa78c6e84142e
fadhilmufid <mufidfadhil@gmail.com>
2022-07-30 11:01:19 +0700

    Print and Put

---
+++ 02_print_and_put.rb
@@ -0,0 +1,10 @@
+#put will print out and and after that reate new line
+puts "Fadhil"
+#but print will only print out without line
+print "Mufid"
+puts "Testing"
+#coba will be in new line
+print "Coba"
+#ini nyambung will be beside the coba 
+print " ini nyambung"
+
